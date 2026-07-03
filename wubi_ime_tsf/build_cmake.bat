0ba8ade66d9cd4bd02b251acef44564d9ece8b7f
johnhzc <john_hzc@126.com>
2026-07-03 17:42:53 +0800

    fix: convert bat files to ANSI (CP936) to fix Chinese garbled output

--- wubi_ime_tsf/build_cmake.bat
+++ wubi_ime_tsf/build_cmake.bat
@@ -1,10 +1,10 @@
 @echo off
-chcp 65001 >nul
+chcp 936 >nul
 echo [1/4] Setting up VS environment...
 call "D:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 echo [2/4] VS environment set. cl.exe path:
 where cl.exe
 echo [3/4] Running CMake...
-cd /d "D:\BeansWorkingSpace\lyt\2026涓北澶у瀛︾敓绗簩璇惧爞\coding\wubi_ime_tsf\build"
+cd /d "D:\BeansWorkingSpace\lyt\2026中山大学学生第二课堂\coding\wubi_ime_tsf\build"
 "..\cmake\bin\cmake.exe" .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe
 echo [4/4] CMake done.
